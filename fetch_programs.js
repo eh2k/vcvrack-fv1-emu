f86726dd6e7f0984e3d713be9ddddab6a797b333
eh2k <eh2k@github.com>
2020-06-06 14:01:03 +0200

    fetch_programs

---
+++ fetch_programs.js
@@ -0,0 +1,43 @@
+const http = require("https")
+const fs = require('fs');
+const crypto = require('crypto');
+
+const fetch = (url) => new Promise((resolve, reject) => {
+    http.get(url, (res) => {
+        let data = '';
+        res.on('end', () => resolve(data));
+        res.on('data', (buf) => data += buf.toString());
+    })
+        .on('error', e => reject(e));
+});
+
+async function main() {
+
+    var s = await fetch("https://raw.githubusercontent.com/mstratman/fv1-programs/master/programs.js");
+    eval(s.replace("export default", "var programs = "));
+
+    var spn_programs = [];
+    for (i = 0; i < programs.length; i++) {
+        if (programs[i]["download"].hasOwnProperty('spn')) {
+            programs[i]["download"]["spn"]["file"] = "https://mstratman.github.io/fv1-programs/files/" + programs[i]["download"]["spn"]["file"];
+
+            var tmp = await fetch(programs[i]["download"]["spn"]["file"])
+            const hash = crypto.createHash('sha256').update(tmp).digest('hex');
+            console.log(hash)
+            const buff = Buffer.from(tmp, 'utf-8');
+            programs[i]["download"]["spn"]["sha256"] = hash;
+            spn_programs.push(programs[i]);
+
+            fs.writeFile(hash + '.spn', tmp, function (err) {
+                if (err) return console.log(err);
+            });
+        }
+    }
+    
+    const programs_json = JSON.stringify(spn_programs, null, "\t");
+    fs.writeFile('programs.json', programs_json, function (err) {
+        if (err) return console.log(err);
+    });
+}
+
+main();
